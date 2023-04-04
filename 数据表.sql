e639e4ad547c355b39ba5ffa8414e01469a9ff4a
Wick.Yao <wick.yao@fwd.com>
2023-04-04 17:44:02 +0800

    测试产品首页的增删改查

--- 数据表.sql
+++ 数据表.sql
@@ -3,16 +3,16 @@
 
 CREATE TABLE `product_info` (
   `id` VARCHAR(64) NOT NULL COMMENT '产品id',
-  `name` VARCHAR(255) NOT NULL COMMENT '产品名字',
-  `description` TEXT NOT NULL COMMENT '描述',
-  `cid` VARCHAR(64) NOT NULL COMMENT '分类id',
-  `status` INT NOT NULL COMMENT '产品状态',
+  `name` VARCHAR(255)  NULL COMMENT '产品名字',
+  `description` TEXT  NULL COMMENT '描述',
+  `cid` VARCHAR(64)  NULL COMMENT '分类id',
+  `status` INT  NULL COMMENT '产品状态',
   `created_date` DATE NOT NULL COMMENT '创建时间',
   `update_date` DATE NOT NULL COMMENT '更新日期',
   `is_deleted` INT NOT NULL COMMENT '是否删除',
-  `sort` INT NOT NULL COMMENT '用于轮播图的排序',
-  `pub_code` VARCHAR(64) NOT NULL COMMENT '产品单号',
-  `weight` INT NOT NULL COMMENT '权重',
+  `sort` INT  NULL COMMENT '用于轮播图的排序',
+  `pub_code` VARCHAR(64)  NULL COMMENT '产品单号',
+  `weight` INT  NULL COMMENT '权重',
   `cover_url` varchar(100) DEFAULT NULL COMMENT '图片地址',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -20,13 +20,13 @@ CREATE TABLE `product_info` (
 产品分类表
 CREATE TABLE `product_category` (
   `id` VARCHAR(64) NOT NULL COMMENT '产品分类id',
-  `name` VARCHAR(255) NOT NULL COMMENT '产品分类名字',
+  `name` VARCHAR(255)  NULL COMMENT '产品分类名字',
   `created_date` DATE NOT NULL COMMENT '创建时间',
   `update_date` DATE NOT NULL COMMENT '更新日期',
   `is_deleted` INT NOT NULL COMMENT '是否删除',
-  `workflow_id` VARCHAR(64) NOT NULL COMMENT '分类对应审批流程id',
-  `workflow_formula` TEXT NOT NULL COMMENT '审批流程公式',
-  `description` TEXT NOT NULL COMMENT '描述',
+  `workflow_id` VARCHAR(64)  NULL COMMENT '分类对应审批流程id',
+  `workflow_formula` TEXT  NULL COMMENT '审批流程公式',
+  `description` TEXT  NULL COMMENT '描述',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -36,13 +36,13 @@ CREATE TABLE `publish_approve` (
   `created_date` DATE NOT NULL COMMENT '创建时间',
   `update_date` DATE NOT NULL COMMENT '更新日期',
   `is_deleted` INT NOT NULL COMMENT '是否删除',
-  `pub_code` VARCHAR(64) NOT NULL COMMENT '产品单号',
-  `product_id` VARCHAR(64) NOT NULL COMMENT '产品id',
-  `approve_status` INT NOT NULL COMMENT '审批状态',
-  `approver` VARCHAR(64) NOT NULL COMMENT '审批人',
-  `product_name` VARCHAR(100) NOT NULL COMMENT '产品名字',
-  `cid` VARCHAR(32) NOT NULL COMMENT '分类id',
-  `approve_time`DATE NOT NULL COMMENT '审批时间',
+  `pub_code` VARCHAR(64)  NULL COMMENT '产品单号',
+  `product_id` VARCHAR(64)  NULL COMMENT '产品id',
+  `approve_status` INT  NULL COMMENT '审批状态',
+  `approver` VARCHAR(64)  NULL COMMENT '审批人',
+  `product_name` VARCHAR(64)  NULL COMMENT '产品名字',
+  `cid` VARCHAR(32)  NULL COMMENT '分类id',
+  `approve_time`DATE  NULL COMMENT '审批时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -53,13 +53,13 @@ CREATE TABLE `subscribe_approve` (
   `created_date` DATE NOT NULL COMMENT '创建时间',
   `update_date` DATE NOT NULL COMMENT '更新日期',
   `is_deleted` INT NOT NULL COMMENT '是否删除',
-  `sub_code` VARCHAR(64) NOT NULL COMMENT '订阅单号',
-  `product_id` VARCHAR(64) NOT NULL COMMENT '产品id',
-  `approve_status` INT NOT NULL COMMENT '审批状态',
-  `approver` VARCHAR(64) NOT NULL COMMENT '审批人',
-  `product_name` VARCHAR(100) NOT NULL COMMENT '产品名字',
-  `cid` VARCHAR(32) NOT NULL COMMENT '分类id',
-  `approve_time`DATE NOT NULL COMMENT '审批时间',
+  `sub_code` VARCHAR(64)  NULL COMMENT '订阅单号',
+  `product_id` VARCHAR(64)  NULL COMMENT '产品id',
+  `approve_status` INT  NULL COMMENT '审批状态',
+  `approver` VARCHAR(64)  NULL COMMENT '审批人',
+  `product_name` VARCHAR(100)  NULL COMMENT '产品名字',
+  `cid` VARCHAR(32)  NULL COMMENT '分类id',
+  `approve_time`DATE NULL COMMENT '审批时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -70,14 +70,14 @@ CREATE TABLE `workflow_subscribe_approve` (
   `created_date` DATE NOT NULL COMMENT '创建时间',
   `update_date` DATE NOT NULL COMMENT '更新日期',
   `is_deleted` INT NOT NULL COMMENT '是否删除',
-  `sub_code` VARCHAR(64) NOT NULL COMMENT '订阅单号',
-  `node_approval_status` INT NOT NULL COMMENT '节点审批状态',
-  `node_approver` VARCHAR(64) NOT NULL COMMENT '节点审批人',
-  `cid` VARCHAR(32) NOT NULL COMMENT '分类id',
-  `user_list` VARCHAR(64) NOT NULL COMMENT 'node节点审批人',
-  `workflow_id` VARCHAR(64) NOT NULL COMMENT '审批流id',
-  `node_id` VARCHAR(64) NOT NULL COMMENT '审批流节点',
-  `approve_time`DATE NOT NULL COMMENT '审批时间'
+  `sub_code` VARCHAR(64)  NULL COMMENT '订阅单号',
+  `node_approval_status` INT  NULL COMMENT '节点审批状态',
+  `node_approver` VARCHAR(64)  NULL COMMENT '节点审批人',
+  `cid` VARCHAR(32)  NULL COMMENT '分类id',
+  `user_list` VARCHAR(64)  NULL COMMENT 'node节点审批人',
+  `workflow_id` VARCHAR(64)  NULL COMMENT '审批流id',
+  `node_id` VARCHAR(64)  NULL COMMENT '审批流节点',
+  `approve_time`DATE  NULL COMMENT '审批时间'
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -87,14 +87,14 @@ CREATE TABLE `workflow_publish_approve` (
   `created_date` DATE NOT NULL COMMENT '创建时间',
   `update_date` DATE NOT NULL COMMENT '更新日期',
   `is_deleted` INT NOT NULL COMMENT '是否删除',
-  `pub_code` VARCHAR(64) NOT NULL COMMENT '发布单号',
-  `node_approve_status` INT NOT NULL COMMENT '节点审批状态',
-  `node_approver` VARCHAR(64) NOT NULL COMMENT '节点审批人',
-  `cid` VARCHAR(32) NOT NULL COMMENT '分类id',
-  `user_list` VARCHAR(64) NOT NULL COMMENT 'node节点审批人',
-  `workflow_id` VARCHAR(64) NOT NULL COMMENT '审批流id',
-  `node_id` VARCHAR(64) NOT NULL COMMENT '审批流节点',
-  `approve_time`DATE NOT NULL COMMENT '审批时间'
+  `pub_code` VARCHAR(64)  NULL COMMENT '发布单号',
+  `node_approve_status` INT  NULL COMMENT '节点审批状态',
+  `node_approver` VARCHAR(64)  NULL COMMENT '节点审批人',
+  `cid` VARCHAR(32)  NULL COMMENT '分类id',
+  `user_list` VARCHAR(64)  NULL COMMENT 'node节点审批人',
+  `workflow_id` VARCHAR(64)  NULL COMMENT '审批流id',
+  `node_id` VARCHAR(64)  NULL COMMENT '审批流节点',
+  `approve_time`DATE  NULL COMMENT '审批时间'
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -106,8 +106,8 @@ CREATE TABLE `product_cover` (
   `update_date` DATE NOT NULL COMMENT '更新日期',
   `is_deleted` INT NOT NULL COMMENT '是否删除',
   `name` varchar(100)  NULL COMMENT '图片名字',
-  `address` VARCHAR(100) NOT NULL COMMENT 'url地址',
-  `status` INT NOT NULL COMMENT '图片转态',
+  `address` VARCHAR(100)  NULL COMMENT 'url地址',
+  `status` INT  NULL COMMENT '图片转态',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
