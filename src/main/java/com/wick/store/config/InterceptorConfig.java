a50aface06239540f3ce2b2cdfbbe1e7aaf53a3f
姚鹏峰 <suggelady@163.com>
2023-04-09 16:31:18 +0800

    解决跨域问题

--- src/main/java/com/wick/store/config/InterceptorConfig.java
+++ src/main/java/com/wick/store/config/InterceptorConfig.java
@@ -1,5 +1,6 @@
 package com.wick.store.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -23,15 +24,21 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 .addPathPatterns("/api/**") //拦截的地址
                 .excludePathPatterns("/user/**"); //不需要拦截的地址，如登录接口
     }
+
+    /**
+     * 跨域
+     * @param registry
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")//设置允许跨域的路径
                 //如果有多个路径需要跨域，只需要将跨域路径放入数组中
                 //String []  allowDomain={"http://**","http://*","http://*"};
                 //.allowedOrigins(allowDomain)//多url跨域
-                .allowedOrigins("localhost:8080/")//设置允许跨域请求的域名
-                .allowCredentials(true)//是否允许证书 不写默认开启
-                .allowedMethods("GET","POST","PUT","OPTIONS","DELETE","PATCH") //设置允许的方法
+                .allowedOrigins("*")//设置允许跨域请求的域名
+                .allowCredentials(false)//是否允许证书 不写默认开启
+                .allowedMethods("GET","POST","PUT","OPTIONS","DELETE","PATCH")
+                .allowedHeaders("*")//设置允许的方法
                 .maxAge(3600);//跨域允许时间
     }
 
